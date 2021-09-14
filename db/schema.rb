6cc6a62ed5c137c2c68c2520bcda28f9f1caa4d1
YongKeeTan <yongkee.t@gmail.com>
2021-09-14 19:05:39 +0800

    adding associations between user and orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,27 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_14_101807) do
+ActiveRecord::Schema.define(version: 2021_09_14_110211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "number"
-    t.string "first_name"
-    t.string "last_name"
-    t.string "current_location"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
 
   create_table "food_orders", force: :cascade do |t|
     t.bigint "food_id", null: false
@@ -57,6 +40,8 @@ ActiveRecord::Schema.define(version: 2021_09_14_101807) do
     t.text "comment"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "restaurants", force: :cascade do |t|
@@ -69,7 +54,24 @@ ActiveRecord::Schema.define(version: 2021_09_14_101807) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "number"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "current_location"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
   add_foreign_key "food_orders", "foods"
   add_foreign_key "food_orders", "orders"
   add_foreign_key "foods", "restaurants"
+  add_foreign_key "orders", "users"
 end
