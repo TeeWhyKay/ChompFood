93a5f0a279c80c4102da04acd1a3f43d5b17266c
YongKeeTan <yongkee.t@gmail.com>
2021-09-14 16:40:42 +0800

    created restaurant model with name validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2021_09_14_083429) do
+=======
+ActiveRecord::Schema.define(version: 2021_09_14_072452) do
+>>>>>>> 4cd3cb0 (created restaurant model with name validation)
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+<<<<<<< HEAD
   create_table "foods", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.float "price"
     t.string "photo_url"
+=======
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.float "lat"
+    t.float "long"
+    t.datetime "opening_time"
+    t.datetime "closing_time"
+>>>>>>> 4cd3cb0 (created restaurant model with name validation)
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
