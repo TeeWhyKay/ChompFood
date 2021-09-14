71e9395bc92f0034569f83af12a68888ed2cb349
jun wei <tjw1993@gmail.com>
2021-09-14 23:47:30 +0800

    trying to merge changes from main branch to my local branch

--- db/schema.rb
+++ db/schema.rb
@@ -15,23 +15,6 @@ ActiveRecord::Schema.define(version: 2021_09_14_101807) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "number"
-    t.string "first_name"
-    t.string "last_name"
-    t.string "current_location"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
   create_table "food_orders", force: :cascade do |t|
     t.bigint "food_id", null: false
     t.bigint "order_id", null: false
@@ -67,6 +50,22 @@ ActiveRecord::Schema.define(version: 2021_09_14_101807) do
     t.datetime "closing_time"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "number"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "current_location"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "food_orders", "foods"
