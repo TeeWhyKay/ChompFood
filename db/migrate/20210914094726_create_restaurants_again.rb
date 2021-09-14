d22c3428690cd40fc99f0be0c67a448b1b37c15d
YongKeeTan <yongkee.t@gmail.com>
2021-09-14 17:52:10 +0800

    added restaurant table to schema

---
+++ db/migrate/20210914094726_create_restaurants_again.rb
@@ -0,0 +1,13 @@
+class CreateRestaurantsAgain < ActiveRecord::Migration[6.0]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.float :lat
+      t.float :long
+      t.datetime :opening_time
+      t.datetime :closing_time
+
+      t.timestamps
+    end
+  end
+end
