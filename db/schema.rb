404e88a8745b3fc4d9bc1edf0dc1343349bde796
YongKeeTan <yongkee.t@gmail.com>
2021-09-14 18:09:30 +0800

    added association between food and restaurant

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_14_094726) do
+ActiveRecord::Schema.define(version: 2021_09_14_100702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2021_09_14_094726) do
     t.string "photo_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "restaurant_id"
+    t.index ["restaurant_id"], name: "index_foods_on_restaurant_id"
   end
 
   create_table "restaurants", force: :cascade do |t|
@@ -34,4 +36,5 @@ ActiveRecord::Schema.define(version: 2021_09_14_094726) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "foods", "restaurants"
 end
