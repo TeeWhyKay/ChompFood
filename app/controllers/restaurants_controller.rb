f0d4fa360908d37e21dd29c72eadbc2e4266c7c1
YongKeeTan <yongkee.t@gmail.com>
2021-09-15 13:05:39 +0800

    added search form on home page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,14 @@
 class RestaurantsController < ApplicationController
   def index
+    if params[:query].present?
+      @restaurants = Restaurant.where(name: params[:query])
+    else
+      @restaurants = Restaurant.all
+    end
+    @restaurants = Restaurant.all
   end
 
   def show
+    @restaurant = Restaurant.find(params[:id])
   end
 end
