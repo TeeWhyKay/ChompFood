a7b546879d9b21257782cdfb14505d7398240915
Min Xuan Ng <hi@ngminxuan.com>
2021-09-15 12:48:50 +0800

    added address column migration

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,7 @@
 class Restaurant < ApplicationRecord
   has_many :foods
   validates :name, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_15_044359) do
+ActiveRecord::Schema.define(version: 2021_09_15_044729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2021_09_15_044359) do
     t.datetime "updated_at", precision: 6, null: false
     t.float "latitude"
     t.float "longitude"
+    t.string "address"
   end
 
   create_table "users", force: :cascade do |t|
