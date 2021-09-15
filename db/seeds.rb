667cde5771664cefcc7579e2392f836afcd51ddb
jun wei <tjw1993@gmail.com>
2021-09-15 11:51:34 +0800

    first commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+
+puts 'Creating 10 fake fooditems...'
+10.times do
+  fooditem = Food.new(
+    name:       Faker::Food.dish,
+    description:    Faker::Food.description,
+    price: rand(0...10.0).round(2),
+    photo_url: "https://source.unsplash.com/random/300x200?sig=#{rand(100)}",
+    restaurant:  
+  )
+  restaurant.save!
+end
+puts 'Finished!'
