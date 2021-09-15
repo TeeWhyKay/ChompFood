2ff59e7eea0e261bd4b7001b6bc3e0d3630439e0
Min Xuan Ng <hi@ngminxuan.com>
2021-09-15 13:05:38 +0800

    editing seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -9,11 +11,13 @@ puts "clearing db"
 Restaurant.destroy_all
 
 puts "seeding db with restaurants"
+
+
+
 20.times do
   Restaurant.create(
     name: Faker::Restaurant.name,
-    lat: Faker::Address.latitude,
-    long: Faker::Address.longitude,
+    address:
     opening_time: ["7:00AM", "8:00AM", "9:00AM", "10:00AM", "11:00AM"].sample,
     closing_time: ["6:00PM", "7:00PM", "8:00PM", "9:00PM", "10:00PM", "11:00PM"].sample
   )
