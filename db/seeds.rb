5548d77124e1ba7c40c21f5298c64d93a6a18bf8
YongKeeTan <yongkee.t@gmail.com>
2021-09-15 11:31:57 +0800

    seeded db with restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "clearing db"
+Restaurant.destroy_all
+
+puts "seeding db with restaurants"
+20.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    lat: Faker::Address.latitude,
+    long: Faker::Address.longitude,
+    opening_time: ["7:00AM", "8:00AM", "9:00AM", "10:00AM", "11:00AM"].sample,
+    closing_time: ["6:00PM", "7:00PM", "8:00PM", "9:00PM", "10:00PM", "11:00PM"].sample
+  )
+end
+puts "seeding restaurant completed"
