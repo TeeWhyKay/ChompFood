62f1e0de4f3fc1dbc5fe735321035d416691afce
YongKeeTan <yongkee.t@gmail.com>
2021-09-16 11:34:40 +0800

    added webpacker modules to do webpacker serve, but doesn't work

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,14 @@
 class RestaurantsController < ApplicationController
   def index
+    # latlong = results.first.coordinates => [48.856614, 2.3522219]  # latitude and longitude
+    # @restaurants = Restaurant.near(latlong, 5, units: :km) # venues within 5 km of a point
     if params[:query].present?
-      @restaurants = Restaurant.where("name ILIKE ?", "%#{params[:query]}%")
+      query_location = Geocoder.search(params[:query]).first.coordinates
+      if query_location.nil?
+        @restaurants = Restaurant.where("name ILIKE ?", "%#{params[:query]}%")
+      else
+
+      end
     else
       @restaurants = Restaurant.all
     end
