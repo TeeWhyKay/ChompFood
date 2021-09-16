043ddbe6be9e8b38df117a3296d3c08a69ed3bff
YongKeeTan <yongkee.t@gmail.com>
2021-09-16 14:57:46 +0800

    fixed seeding issue for when businesses do not specify their opening-closing hours

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,24 +18,31 @@ url = "https://tih-api.stb.gov.sg/content/v1/search/all?dataset=food_beverages&l
 address_serialized = URI.open(url).read
 address_parsed = JSON.parse(address_serialized)
 
-
 20.times do |index|
   address_street = address_parsed["data"]["results"][index]["address"]
   full_address = "#{address_street["block"]} #{address_street["streetName"]}, Singapore"
-
+  time = address_parsed["data"]["results"][index]["businessHour"]
+  if time.empty?
+    opening_time = ["10:00", "10:30", "11:00"].sample
+    closing_time = ["22:00", "22:30", "23:00"].sample
+  else
+    opening_time = time.first["openTime"]
+    closing_time = time.first["closeTime"]
+  end
   Restaurant.create(
     name: address_parsed["data"]["results"][index]["name"],
     address: full_address,
     longitude: address_parsed["data"]["results"][index]["location"]["longitude"],
     latitude: address_parsed["data"]["results"][index]["location"]["latitude"],
-    opening_time: address_parsed["data"]["results"][index]["businessHour"].first["openTime"],
-    closing_time: address_parsed["data"]["results"][index]["businessHour"].first["closeTime"],
+    opening_time: opening_time,
+    closing_time: closing_time,
     rating: address_parsed["data"]["results"][index]["rating"],
     cuisine: address_parsed["data"]["results"][index]["cuisine"]
   )
   puts "seeded #{address_parsed["data"]["results"][index]["name"]}"
 end
 puts "seeding restaurant completed"
+
 
 
 puts 'Creating 10 fake fooditems...'
