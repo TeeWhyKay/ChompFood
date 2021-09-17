a13b1561663d8a6bf806fc86746f3415924e779b
Min Xuan Ng <hi@ngminxuan.com>
2021-09-17 12:10:02 +0800

    updated routes

--- app/javascript/plugins/init_cart.js
+++ app/javascript/plugins/init_cart.js
@@ -1,3 +1,6 @@
+import { post } from "jquery";
+import { csrfToken } from "@rails/ujs";
+
 const initCart = () => {
   const cart = document.querySelector('#cart');
 
@@ -5,8 +8,19 @@ const initCart = () => {
     const noItems = document.querySelector('.no-items')
     // if local storage has an order
     if (window.localStorage.order) {
-      console.log('there is sth in ls');
       cart.classList.remove('d-none')
+      // POST request to db
+      fetch('/my_cart_info', {
+        method: 'POST',
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json",
+          "X-CSRF-Token": csrfToken()
+        },
+        body: window.localStorage.order
+      }).then(response => response.json())
+        .then(data => console.log(data));
+
     } else {
       noItems.classList.remove('d-none')
     }
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :restaurants, only: [:index, :show]
-  get 'my_cart', to: 'food_orders#show', as: :cart
+  get 'cart', to: 'food_orders#show', as: :cart
+  post 'cart_info', to: 'food_orders#cart_info', as: :cart_info
   get 'restaurants/:id', to: 'food_orders#show', as: :cart_popup
 end
